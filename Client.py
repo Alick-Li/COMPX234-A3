281d122a52f7fbc37a748b69f9d062f764af8444
Alick-Li <alickli@outlook.com>
2025-05-02 17:10:30 +0800

    7th

--- Client.py
+++ Client.py
@@ -11,6 +11,29 @@ def client_task(hostname, port_number, pathname):
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (hostname, port_number)
         client_socket.connect(server_address)
+
+        with open(pathname, 'r') as file:
+            for line in file:
+                command = line.strip().split(' ')[0]
+                key = line.strip().split(' ')[1]
+                if command == 'PUT':
+                    value = line.strip().split(' ')[2:]
+                else:
+                    value = None
+
+                if len(key + ' ' + value) <= 970:
+                    if command == 'READ':
+                        request_message = f"f'{(6 + len(key)):03d}' R {key}"
+                    elif command == 'GET':
+                        request_message = f"f'{(6 + len(key)):03d}' G {key}"
+                    elif command == 'PUT':
+                        request_message = f"f'{(7 + len(key) + len(value)):03d}' P {key} {value}"
+                else:
+                    print('Error: Collated size exceed 970 characters')
+                client_socket.sendall(request_message.encode('utf-8'))
+
+                response_message = client_socket.recv(1024).decode('utf-8')
+                print(f'Received: {response_message}')
     except Exception as e:
         print(f"Error: {e}")
     finally:
